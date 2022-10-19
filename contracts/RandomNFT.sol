3e584d1d3c6d35de29192cd194379594d06228b5
cherrypopsicle <majd.khoury93@gmail.com>
2022-10-19 22:56:42 +0400

    switch

--- contracts/RandomNFT.sol
+++ contracts/RandomNFT.sol
@@ -4,11 +4,19 @@ pragma solidity ^0.8;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Zerise is ERC721 {
-    uint256 public tokenId;
+    uint256 public tokenId = 1;
+    address public cerise = 0xe0110C6EE2138Ecf9962a6f9f6Ad329cDFE1FA17;
 
-    constructor() ERC721("RandomNFT", "ITM") {}
+    event Minted(address indexed user, uint256 indexed tokenId);
+
+    constructor() ERC721("Zerise", "Zse") {}
 
     function bop() public {
         _mint(msg.sender, tokenId++);
+        emit Minted(msg.sender, tokenId);
+    }
+
+    function somethingSwitched() public payable {
+        payable(cerise).transfer(address(this).balance);
     }
 }
